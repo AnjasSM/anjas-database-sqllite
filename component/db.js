cadebb8e4e3320bb2dc0319d38fa15ead3e1a49e
AnjasSM <syarifudin.anjas@gmail.com>
2019-05-09 16:24:57 +0700

    refactoring folder

--- component/db.js
+++ component/db.js
@@ -1,4 +1,4 @@
 const sqlite3  = require('sqlite3').verbose();
-const db       = new sqlite3.Database('./src/db/contacts.db');
+const db       = new sqlite3.Database('./db/contacts.db');
 
 module.exports = db;
